180097ebad7948c420c5c11a5b1131498da06ac0
alastair38 <alastair@alastaircox.com>
2024-03-18 13:15:16 +0000

    + login-form pattern

---
+++ patterns/login-form.php
@@ -0,0 +1,12 @@
+<?php
+/**
+ * Title: Login form
+ * Slug: blockhaus/login-form
+ */
+?>
+
+<!-- wp:group {"style":{"spacing":{"padding":{"top":"var:preset|spacing|20","bottom":"var:preset|spacing|20","left":"var:preset|spacing|20","right":"var:preset|spacing|20"},"margin":{"top":"var:preset|spacing|40","bottom":"var:preset|spacing|40"}}},"className":"is-style-default login-form","layout":{"type":"constrained"},"metadata":{"name":"Login form"}} -->
+<div class="wp-block-group is-style-default login-form" style="margin-top:var(--wp--preset--spacing--40);margin-bottom:var(--wp--preset--spacing--40);padding-top:var(--wp--preset--spacing--20);padding-right:var(--wp--preset--spacing--20);padding-bottom:var(--wp--preset--spacing--20);padding-left:var(--wp--preset--spacing--20)"><!-- wp:loginout {"displayLoginAsForm":true,"style":{"layout":{"selfStretch":"fit","flexSize":null}}} /-->
+
+<!-- wp:blockhaus/lost-password {"name":"blockhaus/lost-password","align":"left","mode":"preview"} /--></div>
+<!-- /wp:group -->
