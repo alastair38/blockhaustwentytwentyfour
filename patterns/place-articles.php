bb29409b851f68dcb8be46eb88582b755015a4fe
alastair38 <alastair@alastaircox.com>
2024-03-25 10:06:38 +0000

    ~ post-title to h3 in place-articles and place-resources patterns

--- patterns/place-articles.php
+++ patterns/place-articles.php
@@ -9,34 +9,40 @@
 <!-- wp:group {"align":"wide","style":{"spacing":{"margin":{"top":"var:preset|spacing|50","bottom":"var:preset|spacing|50"}, "blockGap":"var:preset|spacing|30"}},"backgroundColor":"accent-2","className":"is-style-spacing-group","layout":{"type":"default"},"metadata":{"name":"Place Articles"}} -->
 <div class="wp-block-group alignwide is-style-spacing-group has-accent-2-background-color has-background" style="margin-top:var(--wp--preset--spacing--50);margin-bottom:var(--wp--preset--spacing--50)">
 
-<!-- wp:heading {"style":{"typography":{"fontStyle":"normal","fontWeight":"900"}},"fontSize":"large"} -->
-<h2 class="wp-block-heading has-large-font-size" style="font-style:normal;font-weight:900">
-  <?php echo esc_html_x( 'Articles', 'Title text of the Place Articles section', 'blockhaus' ); ?>
-</h2>
-<!-- /wp:heading -->
+  <!-- wp:heading {"style":{"typography":{"fontStyle":"normal","fontWeight":"900"}},"fontSize":"large"} -->
+  <h2 class="wp-block-heading has-large-font-size" style="font-style:normal;font-weight:900">
+    <?php echo esc_html_x( 'Articles', 'Title text of the Place Articles section', 'blockhaus' ); ?>
+  </h2>
+  <!-- /wp:heading -->
 
-<!-- wp:paragraph {"fontSize":"medium"} -->
-<p class="has-medium-font-size">
-  <?php echo esc_html_x( 'Here are the latest blog posts from the team.', 'Paragraph text of the Place Articles section', 'blockhaus' ); ?>
-</p>
-<!-- /wp:paragraph -->
+  <!-- wp:paragraph {"fontSize":"medium"} -->
+  <p class="has-medium-font-size">
+    <?php echo esc_html_x( 'Here are the latest blog posts from the team.', 'Paragraph text of the Place Articles section', 'blockhaus' ); ?>
+  </p>
+  <!-- /wp:paragraph -->
 
-<!-- wp:query {"queryId":2,"query":{"perPage":3,"pages":0,"offset":0,"postType":"post","order":"desc","orderBy":"date","author":"","search":"","exclude":[],"sticky":"","inherit":false,"taxQuery":{"place-taxonomy":[15],"location":[8]}}} -->
-<div class="wp-block-query"><!-- wp:post-template {"style":{"spacing":{"blockGap":"0"}},"className":"is-style-grid-template","layout":{"type":"default","columnCount":1}} -->
-<!-- wp:group {"style":{"dimensions":{"minHeight":"100%"},"spacing":{"padding":{"top":"var:preset|spacing|10","bottom":"var:preset|spacing|10","left":"var:preset|spacing|10","right":"var:preset|spacing|10"}}},"backgroundColor":"base-2","className":"is-style-shadow-group","layout":{"type":"flex","orientation":"vertical"}} -->
-<div class="wp-block-group is-style-shadow-group has-base-2-background-color has-background" style="min-height:100%;padding-top:var(--wp--preset--spacing--10);padding-right:var(--wp--preset--spacing--10);padding-bottom:var(--wp--preset--spacing--10);padding-left:var(--wp--preset--spacing--10)"><!-- wp:post-featured-image {"aspectRatio":"16/9","width":"100%","height":"12em","style":{"border":{"radius":"6px"},"layout":{"selfStretch":"fixed","flexSize":"12em"}}} /-->
+  <!-- wp:query {"queryId":2,"query":{"perPage":3,"pages":0,"offset":0,"postType":"post","order":"desc","orderBy":"date","author":"","search":"","exclude":[],"sticky":"","inherit":false,"taxQuery":{"place-taxonomy":[15],"location":[8]}}} -->
+  <div class="wp-block-query">
+    
+    <!-- wp:post-template {"style":{"spacing":{"blockGap":"0"}},"className":"is-style-grid-template","layout":{"type":"default","columnCount":1}} -->
+      <!-- wp:group {"style":{"dimensions":{"minHeight":"100%"},"spacing":{"padding":{"top":"var:preset|spacing|10","bottom":"var:preset|spacing|10","left":"var:preset|spacing|10","right":"var:preset|spacing|10"}}},"backgroundColor":"base-2","className":"is-style-shadow-group","layout":{"type":"flex","orientation":"vertical"}} -->
+      <div class="wp-block-group is-style-shadow-group has-base-2-background-color has-background" style="min-height:100%;padding-top:var(--wp--preset--spacing--10);padding-right:var(--wp--preset--spacing--10);padding-bottom:var(--wp--preset--spacing--10);padding-left:var(--wp--preset--spacing--10)">
+        <!-- wp:post-featured-image {"aspectRatio":"16/9","width":"100%","height":"12em","style":{"border":{"radius":"6px"},"layout":{"selfStretch":"fixed","flexSize":"12em"}}} /-->
+        <!-- wp:post-title {"level":3,"style":{"typography":{"fontStyle":"normal","fontWeight":"700","fontSize":"1.25rem"}}} /-->
+        <!-- wp:blockhaus/read-more {"name":"blockhaus/read-more","data":{},"mode":"preview"} /-->
+      </div>
+      <!-- /wp:group -->
+    <!-- /wp:post-template -->
 
-<!-- wp:post-title {"style":{"typography":{"fontStyle":"normal","fontWeight":"700","fontSize":"1.25rem"}}} /-->
+    <!-- wp:query-no-results -->
+      <!-- wp:paragraph -->
+      <p>
+        <?php echo esc_html_x( 'We are regularly adding content. Please check back soon.', 'No results text of the Place Articles section', 'blockhaus' ); ?>
+      </p>
+      <!-- /wp:paragraph -->
+    <!-- /wp:query-no-results -->
+  </div>
+  <!-- /wp:query -->
 
-<!-- wp:blockhaus/read-more {"name":"blockhaus/read-more","data":{},"mode":"preview"} /-->
 </div>
-<!-- /wp:group -->
-<!-- /wp:post-template -->
-
-<!-- wp:query-no-results -->
-<!-- wp:paragraph -->
-<p><?php echo esc_html_x( 'We are regularly adding content. Please check back soon.', 'No results text of the Place Articles section', 'blockhaus' ); ?></p>
-<!-- /wp:paragraph -->
-<!-- /wp:query-no-results --></div>
-<!-- /wp:query --></div>
 <!-- /wp:group -->
--- patterns/place-resources.php
+++ patterns/place-resources.php
@@ -7,37 +7,53 @@
 ?>
 
 <!-- wp:group {"align":"wide","style":{"spacing":{"blockGap":"var:preset|spacing|30"}},"className":"is-style-default","layout":{"type":"default"},"metadata":{"name":"Place Resources"}} -->
-<div class="wp-block-group alignwide is-style-default"><!-- wp:heading {"style":{"typography":{"fontStyle":"normal","fontWeight":"900"}},"fontSize":"large"} -->
-<h2 class="wp-block-heading has-large-font-size" style="font-style:normal;font-weight:900"><?php echo esc_html_x( 'Resources', 'Title text of the Place Resources section', 'blockhaus' ); ?></h2>
-<!-- /wp:heading -->
+<div class="wp-block-group alignwide is-style-default">
+  
+  <!-- wp:heading {"style":{"typography":{"fontStyle":"normal","fontWeight":"900"}},"fontSize":"large"} -->
+  <h2 class="wp-block-heading has-large-font-size" style="font-style:normal;font-weight:900">
+    <?php echo esc_html_x( 'Resources', 'Title text of the Place Resources section', 'blockhaus' ); ?>
+  </h2>
+  <!-- /wp:heading -->
 
-<!-- wp:query {"queryId":2,"query":{"perPage":3,"pages":0,"offset":0,"postType":"resources","order":"desc","orderBy":"date","author":"","search":"","exclude":[],"sticky":"","inherit":false,"taxQuery":{"place-taxonomy":[15]},"parents":[]}} -->
-<div class="wp-block-query"><!-- wp:post-template {"style":{"spacing":{"blockGap":"0"}},"className":"is-style-grid-template","layout":{"type":"default","columnCount":3}} -->
-<!-- wp:group {"style":{"spacing":{"blockGap":"var:preset|spacing|10"},"dimensions":{"minHeight":"100%"}},"backgroundColor":"base-2","className":"is-style-shadow-group","layout":{"type":"flex","orientation":"vertical"}} -->
-<div class="wp-block-group is-style-shadow-group has-base-2-background-color has-background" style="min-height:100%"><!-- wp:group {"layout":{"type":"default"}} -->
-<div class="wp-block-group"><!-- wp:post-title {"style":{"typography":{"fontStyle":"normal","fontWeight":"700"}},"fontSize":"medium"} /-->
+  <!-- wp:query {"queryId":2,"query":{"perPage":3,"pages":0,"offset":0,"postType":"resources","order":"desc","orderBy":"date","author":"","search":"","exclude":[],"sticky":"","inherit":false,"taxQuery":{"place-taxonomy":[15]},"parents":[]}} -->
+  <div class="wp-block-query">
+    <!-- wp:post-template {"style":{"spacing":{"blockGap":"0"}},"className":"is-style-grid-template","layout":{"type":"default","columnCount":3}} -->
+      <!-- wp:group {"style":{"spacing":{"blockGap":"var:preset|spacing|10"},"dimensions":{"minHeight":"100%"}},"backgroundColor":"base-2","className":"is-style-shadow-group","layout":{"type":"flex","orientation":"vertical"}} -->
+      <div class="wp-block-group is-style-shadow-group has-base-2-background-color has-background" style="min-height:100%">
+      
+        <!-- wp:group {"layout":{"type":"default"}} -->
+        <div class="wp-block-group">
+          <!-- wp:post-title {"level":3,"style":{"typography":{"fontStyle":"normal","fontWeight":"700"}},"fontSize":"medium"} /-->
 
-<!-- wp:separator {"style":{"color":{"background":"#d5d5d5"}},"className":"is-style-wide"} -->
-<hr class="wp-block-separator has-text-color has-alpha-channel-opacity has-background is-style-wide" style="background-color:#d5d5d5;color:#d5d5d5"/>
-<!-- /wp:separator --></div>
-<!-- /wp:group -->
+          <!-- wp:separator {"style":{"color":{"background":"#d5d5d5"}},"className":"is-style-wide"} -->
+          <hr class="wp-block-separator has-text-color has-alpha-channel-opacity has-background is-style-wide" style="background-color:#d5d5d5;color:#d5d5d5"/>
+          <!-- /wp:separator -->
+        </div>
+        <!-- /wp:group -->
 
-<!-- wp:blockhaus/read-more {"name":"blockhaus/read-more","data":{},"mode":"preview"} /--></div>
-<!-- /wp:group -->
-<!-- /wp:post-template -->
+        <!-- wp:blockhaus/read-more {"name":"blockhaus/read-more","data":{},"mode":"preview"} /-->
+      </div>
+      <!-- /wp:group -->
+    <!-- /wp:post-template -->
 
-<!-- wp:query-pagination -->
-<!-- wp:query-pagination-previous /-->
+    <!-- wp:query-pagination -->
+      <!-- wp:query-pagination-previous /-->
 
-<!-- wp:query-pagination-numbers /-->
+      <!-- wp:query-pagination-numbers /-->
 
-<!-- wp:query-pagination-next /-->
-<!-- /wp:query-pagination -->
+      <!-- wp:query-pagination-next /-->
+    <!-- /wp:query-pagination -->
 
-<!-- wp:query-no-results -->
-<!-- wp:paragraph {"placeholder":"Add text or blocks that will display when a query returns no results."} -->
-<p><?php echo esc_html_x( 'We are regularly adding content. Please check back soon.', 'No results text of the Place Resources section', 'blockhaus' ); ?></p>
-<!-- /wp:paragraph -->
-<!-- /wp:query-no-results --></div>
-<!-- /wp:query --></div>
+    <!-- wp:query-no-results -->
+    <!-- wp:paragraph {"placeholder":"Add text or blocks that will display when a query returns no results."} -->
+    <p>
+      <?php echo esc_html_x( 'We are regularly adding content. Please check back soon.', 'No results text of the Place Resources section', 'blockhaus' ); ?>
+    </p>
+    <!-- /wp:paragraph -->
+    <!-- /wp:query-no-results -->
+    
+  </div>
+  <!-- /wp:query -->
+
+</div>
 <!-- /wp:group -->
