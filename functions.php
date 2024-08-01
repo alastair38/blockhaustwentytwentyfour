c9a88474c6a3fe256f3bb52665631071c5b816c5
alastair38 <alastair@alastaircox.com>
2024-08-01 14:09:30 +0100

    + favicons

--- functions.php
+++ functions.php
@@ -479,6 +479,24 @@ function blockhaus_add_to_head() {
 		endif;
 		
 	endif;
-	
+
 }
 add_action( 'wp_head', 'blockhaus_add_to_head' );
+
+function blockhaus_favicon() {
+	echo '<link rel="apple-touch-icon" sizes="180x180" href="' . get_template_directory_uri() . '/apple-touch-icon.png">
+	<link rel="icon" type="image/png" sizes="32x32" href="' . get_template_directory_uri() . '/favicon-32x32.png">
+	<link rel="icon" type="image/png" sizes="16x16" href="' . get_template_directory_uri() . '/favicon-16x16.png">
+	<link rel="manifest" href="' . get_template_directory_uri() . '/site.webmanifest">
+	<link rel="mask-icon" href="' . get_template_directory_uri() . '/safari-pinned-tab.svg" color="#5bbad5">
+	<meta name="msapplication-TileColor" content="#da532c">
+	<meta name="theme-color" content="#ffffff">';
+	
+}
+
+add_action( 'wp_head', 'blockhaus_favicon' );
+
+function favicon4admin() {
+  echo '<link rel="Shortcut Icon" type="image/x-icon" href="' . get_template_directory_uri() . '/favicon.ico" />';
+  }
+  add_action( 'admin_head', 'favicon4admin' );
